a619443a78ab56b7c4fccec17705e36939100a9a
Lukas Guenther <l.guenther@leuchtfeuer.com>
2020-07-29 10:10:41 +0200

    [bugfix] added fallback helper

---
+++ Helper/CitrixHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace MauticPlugin\MauticCitrixBundle\Helper;
+
+class CitrixHelper
+{
+    /**
+     * This is necessary because the core Code needs one of the refactored GoToBundle-Classes
+     *
+     * @param $integration
+     * @return bool
+     */
+    public static function isAuthorized($integration)
+    {
+        return \MauticPlugin\MauticGoToBundle\Helper\GoToHelper::isAuthorized($integration);
+    }
+}
