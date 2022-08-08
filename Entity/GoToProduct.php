a48652719d8d3d625c5e6ff775a897901bac3308
Jan Kozak <galvani78@gmail.com>
2022-08-08 10:24:06 +0200

    fix duplicate getter/setter

--- Entity/GoToProduct.php
+++ Entity/GoToProduct.php
@@ -112,22 +112,6 @@ class GoToProduct implements \JsonSerializable
     /**
      * @return mixed
      */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id): void
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getProductKey()
     {
         return $this->product_key;
